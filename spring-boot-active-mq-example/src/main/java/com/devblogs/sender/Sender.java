f3f850951143ae32010788a6d35c5a739a27339e
zheka <user@dev-blogs.com>
2019-12-11 17:29:10 +0300

    added spring-boot-active-mq-example application

---
+++ spring-boot-active-mq-example/src/main/java/com/devblogs/sender/Sender.java
@@ -0,0 +1,18 @@
+package com.devblogs.sender;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+
+public class Sender {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	public void send(String message) {
+		LOGGER.info("sending message='{}'", message);
+		jmsTemplate.convertAndSend("test.q", message);
+	}
+}
